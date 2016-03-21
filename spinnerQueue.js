83061b1e819e43995ff38183f4ee2ba06b963218
Romms <rommssh@gmail.com>
2016-03-21 12:52:11 +0200

    Added files via upload

---
+++ spinnerQueue.js
@@ -0,0 +1,96 @@
+(function( $ ) {
+    $.widget( "my.spinnerQueue", {
+ 
+        options: { 
+            'queue' : {},
+            'showSpeed' : undefined,
+            'hideSpeed' : undefined,
+        },
+ 
+        _create: function() {
+            $(this.element).append(''+
+                '<div class="spinnerQueue">'+
+                '   <div class="spinner-bg">'+
+                '       <div class="spinner-image"></div>'+
+                '   </div>' +
+                '</div>'
+            );
+
+            this._hide();
+        },
+
+        destroy: function() {
+            $(this.element).find('.spinnerQueue').remove();
+        },
+        
+        started : function(eventName, isCumulative){
+            var value = this.options.queue[eventName];
+
+            if(undefined === value){
+                if(true === isCumulative){
+                    value = 1;
+                } else {
+                    value = true;
+                }
+            } else {
+                if(true === isCumulative){
+                    value++;
+                } else {
+                    // The value isn't changing.
+                }
+            }
+
+            this.options.queue[eventName] = value;
+
+            if( 1 <= this.queueSize() ){
+                this._show();
+            }
+        },
+
+        finished : function(eventName){
+            if(undefined !== this.options.queue[eventName]){
+                if(this.options.queue[eventName] !== true) {
+                    this.options.queue[eventName]--;
+                }
+
+                if(this.options.queue[eventName] === true || this.options.queue[eventName] < 1){
+                    delete this.options.queue[eventName];
+                }
+            }
+
+            if( 0 == this.queueSize()  ){
+                this._hide();
+                this._trigger('queueEnded');
+            }
+        },
+
+        isInQueue : function(eventName){
+            return undefined === this.options.queue[eventName];
+        },
+
+        isQueueEmpty : function(){
+            return 0 == this.queueSize();
+        },
+
+        queueSize : function(){
+            return Object.keys( this.options.queue ).length;
+        },
+
+        _show : function(){
+            var body = document.body;
+            var html = document.documentElement;
+
+            var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+            $(this.element).find('.spinnerQueue').stop().fadeIn( this.options.showSpeed );
+            $(this.element).find('.spinnerQueue').css('height', height + 'px');
+        },
+
+        _hide : function(){
+            $(this.element).find('.spinnerQueue').stop().fadeOut( this.options.hideSpeed );
+        },
+
+    });
+
+}( jQuery ) );
